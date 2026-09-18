887c244c38bf1acc872c96d834d239ec888e44a8
LimChaeyeon0629 <ssjm0629@gmail.com>
2026-09-18 16:23:35 +0900

    fix: 버튼 위치 수정

--- moit-v3/front/pages/admin/report/[reportId].js
+++ moit-v3/front/pages/admin/report/[reportId].js
@@ -288,6 +288,16 @@ function ReportDetailPage() {
                         관리자 신고 상세보기
                     </Title>
 
+                    <Space>
+                        <Button onClick={() => router.push('/admin/report')}>
+                            목록
+                        </Button>
+
+                        <Button onClick={handleTargetView}>
+                            해당 글 보기
+                        </Button>
+                    </Space>
+
                     <Descriptions
                         bordered
                         column={1}
@@ -436,19 +446,6 @@ function ReportDetailPage() {
                     )}
 
                     <div className="admin-report-actions">
-
-                        <div className="admin-report-actions-left">
-                            <Button
-                                onClick={() => router.push('/admin/report')}
-                            >
-                                목록
-                            </Button>
-
-                            <Button onClick={handleTargetView}>
-                                해당 글 보기
-                            </Button>
-                        </div>
-
                         {currentReport.status === 'PENDING' && (
                             <div className="admin-report-actions-right">
                                 <Button
